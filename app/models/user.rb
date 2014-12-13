02c33546071a06a472f3ebb4bb18ee070f1a5c32
Arc Angelo Ibalio <arcangeloibalio@Managers-Mac-mini.local>
2014-12-13 10:13:51 +0800

    Fixed database

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
 
 	geocoded_by :address
 	after_validation :geocode
+     
+        validates :address, presence: true
+        validates :address, length: { minimum: 10 }
+        validates :address, length: { maximum: 500 }
 end
